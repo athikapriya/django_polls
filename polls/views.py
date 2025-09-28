0e01914084cbf852630edbb6563305e8d0352aec
athikapriya <athikapriya1997@gmail.com>
2025-09-28 13:05:05 +0600

    Add detail, result, vote views and update index view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,17 @@
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello World! You are in the poll index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+
+def details(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the result of question %s."
+    return HttpResponse(response % question_id)
+
+def votes(request, question_id):
+    return HttpResponse("You are voting on question %s." % question_id)
